037700cf71d5bb7aa69456205218e3abb6fa0da9
SH0123 <sanghyoboy@naver.com>
2022-09-20 23:25:31 +0900

    Feat: Home 화면 주소 버튼 추가, 색상 추가

--- NEMO-fourcut-map-ios/ViewController.swift
+++ NEMO-fourcut-map-ios/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private lazy var addressButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("주소", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.white
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.cornerRadius = 20
+        button.layer.shadowOffset = CGSize(width: 0, height: 5)
+        button.layer.shadowOpacity = 0.1
+        return button
+    }()
+    
+    private let downArrowImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "chevron.down")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.systemBackground
+        view.addSubview(addressButton)
+        view.addSubview(downArrowImage)
+        addressButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(44)
+        }
+        
+        downArrowImage.snp.makeConstraints {
+            $0.height.equalTo(15)
+            $0.centerY.equalTo(addressButton.snp.centerY)
+            $0.trailing.equalTo(addressButton.snp.trailing).offset(-20)
+        }
     }
 
 
