cc4a67e99d3c2f4f76e5987cdf2abc0ab33b8b92
SH0123 <sanghyoboy@naver.com>
2023-01-12 15:25:58 +0900

    Feat: complete detail review box UI

---
+++ NEMO-fourcut-map-ios/Presenter/DetailStore/Views/ReviewProgressStack.swift
@@ -0,0 +1,22 @@
+//
+//  ReviewProgressStack.swift
+//  NEMO-fourcut-map-ios
+//
+//  Created by sanghyo on 2023/01/12.
+//
+
+import UIKit
+
+final class ReviewProgressStack: UIStackView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+
+}
+
+private extension ReviewProgressStack {
+    
+}
+
