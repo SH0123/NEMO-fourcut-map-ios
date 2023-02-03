81a41a4b41481aa6c3033e8b60b162867ab7d5fb
SH0123 <sanghyoboy@naver.com>
2023-02-03 21:56:05 +0900

    Feat: edit store view controller skeleton

--- NEMO-fourcut-map-ios/Presenter/EditStore/Controllers/EditStoreViewController.swift
+++ NEMO-fourcut-map-ios/Presenter/EditStore/Controllers/EditStoreViewController.swift
@@ -5,4 +5,46 @@
 //  Created by sanghyo on 2023/01/15.
 //
 
-import Foundation
+import UIKit
+
+final class EditStoreViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // 가게 정보 라벨 생성
+    // 디바이더 2개 생성
+    // 체크 박스 라인여러개 생성
+    // 체크 박스 라인들 포함하는 스택 생성
+    // 라디오 버튼 라인 생성
+    // textView 생성
+    // 커스텀 버튼 view에 따로 구현후 생성
+}
+
+private extension EditStoreViewController {
+    
+    func addSubview() {
+        
+    }
+    
+    func setUpUI() {
+        
+    }
+}
+
+// SetUpUI
+private extension EditStoreViewController {
+    
+}
+
+#if DEBUG
+import SwiftUI
+struct EditStoreViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            
+        }
+    }
+}
+#endif
